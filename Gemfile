565fb3476cb6e4e1f7912729763da77477c68cd2
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-05-14 15:40:37 -0700

    added get_page and scraper method

--- Gemfile
+++ Gemfile
@@ -1,12 +1,10 @@
 source "https://rubygems.org"
 #
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-
-
+# git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'pry'
 gem 'nokogiri', '1.8.2'
 gem 'rspec'
 
 # Specify your gem's dependencies in top_countries_to_visit_2018.gemspec
-gemspec
+# gemspec
--- lib/country_scrapper.rb
+++ lib/country_scrapper.rb
@@ -1,12 +1,22 @@
+#!/usr/bin/env ruby
 require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
 class Scraper
 
-  def self.page
-     # doc = Nokogiri::HTML(open(https://www.lonelyplanet.com/best-in-travel/countries)
-     puts "hello"
+
+   def get_page
+      doc = Nokogiri::HTML(open("https://www.lonelyplanet.com/best-in-travel/countries")
+      doc
+    end
+
+    def countries
+      countries = self.get_page.css("marketing_article_header").css("h1")
+      countries.each_with_index |country, index|
+        puts "#{index+1}. country"
+      end 
   end
+
 
 end
